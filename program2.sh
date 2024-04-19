13a7a06960d36dae7fdb6540eae4dd05a4f1bf87
samsimon665 <148468331+samsimon665@users.noreply.github.com>
2024-04-19 15:25:12 +0530

    Add files via upload

---
+++ program2.sh
@@ -0,0 +1,15 @@
+echo "enter the basic salary"
+read b
+hra=0
+da=0
+g=0
+
+if [$b < 15000 ]
+hra =$(($b*02))
+da =$(($b*0.98))
+g=$(($hra+da))
+echo"gross salary =$g"
+fi
+
+if
+                                                                                                                         
